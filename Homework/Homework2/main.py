dbef4dcac52bcdd5845a5e66dbdbc1c410089e5b
tguenther <tdgww4@mst.edu>
2013-10-06 11:06:09 -0500

    adding a treelib library we may need; some test code in main.py

--- Homework/Homework2/main.py
+++ Homework/Homework2/main.py
@@ -7,5 +7,21 @@
 #  CS 437 - Web Data and XML
 #  Homework 2
 
+from treelib import Tree
+from BeautifulSoup import BeautifulSoup
+
+import urllib2
+from urlparse import urlparse, urlunparse
+
+def main():
+   tree = Tree();
+   req = urllib2.urlopen('http://www.cs.toronto.edu')
+   contents = req.read()
+   try:
+      soup = BeautifulSoup(contents)
+      title = soup.title.string
+   except:  # not html
+      pass
+
 if __name__ == '__main__':
-   pass;
+   main();
